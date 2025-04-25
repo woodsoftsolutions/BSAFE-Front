fb53916f70a80f34c41810b3a1e9c1b99794a576
Bladimir Aponte <bladimirapon@gmail.com>
2025-04-24 23:29:33 -0400

    cambios locales

--- bsafe-dashboard/src/app/(full-width-pages)/layout.tsx
+++ bsafe-dashboard/src/app/(full-width-pages)/layout.tsx
@@ -1,15 +1,9 @@
 import React from 'react';
 
-export default function FullWidthPageLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function FullWidthPageLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body>
-        {children}
-      </body>
-    </html>
+    <div>
+      {children}
+    </div>
   );
 }
