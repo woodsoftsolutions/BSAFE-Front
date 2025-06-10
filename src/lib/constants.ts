8793572283a8eaeff4cda10599619a82e37c4ac3
Bladimir Aponte <bladimirapon@gmail.com>
2025-06-10 17:01:00 -0400

    adding envinronments variables .

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,4 @@
 // Central place for environment-based constants
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://5.182.17.212:8080/";
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://5.182.17.212:8080";
 
 //export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8000";
