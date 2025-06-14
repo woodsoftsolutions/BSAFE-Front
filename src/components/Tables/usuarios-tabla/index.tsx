ba5f4b4c66db37ad5004a78ff982716696ac2648
Woodsoft <woodsoftsolutions@gmail.com>
2025-06-14 23:48:18 +0200

    fix 3 users

--- src/components/Tables/usuarios-tabla/index.tsx
+++ src/components/Tables/usuarios-tabla/index.tsx
@@ -43,7 +43,7 @@ export function UsuariosTabla() {
     fetchData();
   };
 
-  const handleEdit = (client: any) => {
+  const handleEdit = (user: any) => {
     setSelectedUser(user);
     setShowEdit(true);
   };
