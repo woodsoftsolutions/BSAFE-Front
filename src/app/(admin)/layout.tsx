943e3c0ba4d62f1586b9a67d1234bb66f253ba61
Woodsoft <woodsoftsolutions@gmail.com>
2025-06-14 12:56:38 +0200

    style pages width

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({ children }: PropsWithChildren) {
         <div className="flex min-h-screen">
           <Sidebar />
 
-          <div className="w-full bg-gray-2 dark:bg-[#020d1a]">
+          <div className="w-full bg-gray-2 dark:bg-[#020d1a]" style="width:80%">
             <Header />
 
             <main className="isolate mx-auto w-full max-w-screen-2xl overflow-hidden p-4 md:p-6 2xl:p-10">
